fc52ac44cf10c7e71d1a91b35e097050d0772447
refendsuccess <59425883+refendsuccess@users.noreply.github.com>
2021-12-25 23:17:16 +0700

    Add files via upload

---
+++ assets/foto/daftarfw.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DaftarFw extends StatefulWidget {
+  @override
+  _DaftarFwState createState() => _DaftarFwState();
+}
+
+class _DaftarFwState extends State<DaftarFw> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
